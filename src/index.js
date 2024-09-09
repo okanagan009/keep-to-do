748aad1866961b0ebc77d9f2407cba7330b56805
okanagan009 <serezharostov161@gmail.com>
2024-09-10 07:46:12 +0800

    adding a task after clicking the button

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // ---------- tab ---------
-
-let tablinks = document.querySelectorAll('.tablinks');
-let tabContent = document.querySelectorAll('.main__section');
+const tablinks = document.querySelectorAll('.tablinks');
+const tabContent = document.querySelectorAll('.main__section');
 
 tablinks.forEach(element => {
     element.addEventListener('click', openTab)
@@ -22,3 +21,21 @@ function openTab(event) {
     document.getElementById(tabName).classList.add('active');
 }
 
+
+// -------------- add-task -------------
+const btnAddTask = document.querySelector('.btn-add-task');
+const blockCreateTask = document.querySelector('.main__task-content');
+const blockCompletedTask = document.querySelector('.main__section--completed-task');
+const blockTrash = document.querySelector('.main__section--trash');
+
+btnAddTask.addEventListener('click', addTask)
+
+function addTask() {
+    btnAddTask.parentElement.style.display = "none";
+
+    const div = document.createElement('div');
+    div.className = 'main__task-text';
+    div.setAttribute('contenteditable', 'true');
+
+    blockCreateTask.appendChild(div);
+}
